08097c1983aa4ef390a6bc055b60b9872bb72864
John Manuvel <sjohnmanuel04@gmail.com>
2025-04-23 15:39:30 +0530

    v1

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const Carts = require('../models/Carts');
+
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -54,6 +56,41 @@ router.get('/profile/:id', async (req, res) => {
     res.status(500).json({ message: 'error fetching user' });
   }
 });
+
+//  Add to Cart
+router.post('/cart', async (req, res) => {
+  const { userId, name, image, price, quantity } = req.body;
+
+  try {
+    const existingItem = await Carts.findOne({ userId, name });
+    if (existingItem) {
+      existingItem.quantity += quantity;
+      await existingItem.save();
+      return res.status(200).json({ message: 'Item updated in cart.' });
+    }
+
+    const newItem = new Carts({ userId, name, image, price, quantity }); //Fixed
+    await newItem.save();
+    res.status(201).json({ message: 'Item added to cart.' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to save item to cart.' });
+  }
+});
+
+//  Get Cart Items by userId
+router.get('/cart/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const cartItems = await Carts.find({ userId });
+    res.status(200).json(cartItems);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to fetch cart items.' });
+  }
+});
+
 
 
 module.exports = router;
